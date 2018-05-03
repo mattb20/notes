5774207d84d8c94d46c62e879f50c3406b340319
Matthew <matt_burgoyne@live.co.uk>
2018-05-03 13:19:53 +0100

    html display method edited, needs to be tested

--- note-list-view.js
+++ note-list-view.js
@@ -4,8 +4,17 @@
   };
 
   ListView.prototype.display = function(){
-
-    return "<ul><li><div>"+(this.list.notelist.map(note => note.slice(0,20)).join("</div></li><li><div>"))+"</div></li></ul>";
+    var htmlString = '<ul><li><div id="0"> <a href="notes/0">';
+    for (var i = 0; i<this.list.notelist.length; i++){
+      var note = this.list.notelist[i].slice(0,20);
+      htmlString += note;
+      if (i !== this.list.notelist.length - 1) {
+        htmlString += `</a></div></li></ul><div id="${i+1}"<a href="notes/${i+1}"`;
+      }
+    }
+    htmlString += "</a></div></li></ul>";
+    return htmlString;
+    // return "<ul><li><div>"+(this.list.notelist.map(note => note.slice(0,20)).join("</div></li><li><div>"))+"</div></li></ul>";
   };
  exports.ListView = ListView;
   })(this);
